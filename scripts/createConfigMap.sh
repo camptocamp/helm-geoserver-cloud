5b15fe115cf7f0396a769890c2d8446a87787128
Andrea Borghi <andrea.borghi@camptocamp.com>
2022-02-21 12:03:26 +0100

    add submodule for geoserver-cloud-config and script to generate the configmap

---
+++ scripts/createConfigMap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+configDir=$(mktemp -d )
+cp submodule_config/*.yml $configDir
+sed -i -E 's/([[:space:]]|\r)+$//g' $configDir/*
+kubectl create configmap TEMPLATE_STRING --from-file=$configDir --dry-run=client -o yaml > templates/config/configmap.yaml
+sed -i 's,TEMPLATE_STRING,{{ include "geoserver.fullname" . }}-config-configs\n  labels:\n    {{- include "geoserver.labels" . | nindent 4 }}\n    app.kubernetes.io/component: "config"\n',g templates/config/configmap.yaml
+rm -r $configDir
