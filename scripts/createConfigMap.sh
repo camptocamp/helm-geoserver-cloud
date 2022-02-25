e0bbff7b614f9accec94a8630d8e5f0cb2dd404a
Andrea Borghi <andrea.borghi@camptocamp.com>
2022-02-25 10:01:24 +0100

    remove hugly shell script

--- scripts/createConfigMap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-configDir=$(mktemp -d )
-cp submodule_config/*.yml $configDir
-sed -i -E 's/([[:space:]]|\r)+$//g' $configDir/*
-kubectl create configmap TEMPLATE_STRING --from-file=$configDir --dry-run=client -o yaml > templates/config/configmap.yaml
-sed -i 's,TEMPLATE_STRING,{{ include "geoserver.fullname" . }}-config-configs\n  labels:\n    {{- include "geoserver.labels" . | nindent 4 }}\n    app.kubernetes.io/component: "config"',g templates/config/configmap.yaml
-rm -r $configDir
